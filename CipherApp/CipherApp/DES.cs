bd1840971b7478eefb6035b95ec7ba3d52cb0afc
r-amaro <ralf.g.amaro@gmail.com>
2021-03-26 01:48:24 -0500

    pad text added.

--- CipherApp/CipherApp/DES.cs
+++ CipherApp/CipherApp/DES.cs
@@ -56,15 +56,49 @@ namespace CipherApp
          * Final inverse permutation
          */
 
+        private static string text;
+        private static string cipherText;
+        private static int[] initialPerm = {58, 50, 42, 34, 26, 18, 10, 2,
+                             60, 52, 44, 36, 28, 20, 12, 4,
+                             62, 54, 46, 38, 30, 22, 14, 6,
+                             64, 56, 48, 40, 32, 24, 16, 8,
+                             57, 49, 41, 33, 25, 17, 9, 1,
+                             59, 51, 43, 35, 27, 19, 11, 3,
+                             61, 53, 45, 37, 29, 21, 13, 5,
+                             63, 55, 47, 39, 31, 23, 15, 7 };
 
         public static string encrypt(string m, int k)
         {
-            string cipherText = "";
+            cipherText = "";
+            text = m;
+            string test = "H";
 
+            m = padText(text);
 
 
             return cipherText;
         }
+
+        private static string padText(string m)
+        {
+            int mod64 = (m.Length * 2) % 64;
+            int charsToPad = (64 - mod64)/2;
+
+            for(int i = 0; i < charsToPad; i++)
+            {
+                m += '\u0000';
+            }
+
+            return m;
+        }
+
+        private static byte[] permutation(byte[] m, int[] array, int n)
+        {
+
+
+            return m;
+        }
+        
 
     }
 }
